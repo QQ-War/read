d0b77fce7cecdfc6c4e3c2e6df28d41e17ccf1e1
hizhongcai <hizhongcai@gmail.com>
2026-01-30 05:31:04 -0500

    Add proxy cache cleanup

--- src/main/kotlin/web/cron/CleanAssets.kt
+++ src/main/kotlin/web/cron/CleanAssets.kt
@@ -19,6 +19,7 @@ class CleanAssets : Runnable {
     companion object {
         private var isdo = false
         private val codesfile = FileUtils.createFolderIfNotExist(appCtx.externalFiles, "assets","codes")
+        private val proxyfile = FileUtils.createFolderIfNotExist(appCtx.externalFiles, "assets","proxy")
         //private val shareJsfile = FileUtils.createFolderIfNotExist(appCtx.externalFiles, "cache","shareJs")
         //private val logsfile = FileUtils.createFolderIfNotExist("logs")
         val logger: Logger = LoggerFactory.getLogger(CleanAssets::class.java)
@@ -41,7 +42,23 @@ class CleanAssets : Runnable {
                         val instant = creationTime.toInstant()
                         val time=(System.currentTimeMillis()-instant.toEpochMilli())/(60*60*24*1000)
                         if(time > 1){
-                            CleanCache2.logger.info("clean ${it.path}")
+                            logger.info("clean code ${it.path}")
+                            it.delete()
+                        }
+                    }
+                }
+            }
+        }
+        kotlin.runCatching {
+            proxyfile.walk().maxDepth(1).forEach {
+                if(it.isFile){
+                    kotlin.runCatching {
+                        val attributes = Files.readAttributes(it.toPath(), BasicFileAttributes::class.java)
+                        val creationTime = attributes.creationTime()
+                        val instant = creationTime.toInstant()
+                        val time=(System.currentTimeMillis()-instant.toEpochMilli())/(60*60*24*1000)
+                        if(time > 30){
+                            logger.info("clean proxy ${it.path}")
                             it.delete()
                         }
                     }
