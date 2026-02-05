6009515dd14bb14ced45bedd96468e5a697247a8
hizhongcai <hizhongcai@gmail.com>
2026-02-05 07:52:15 -0500

    Add separate admin build entry

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/new/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin/index.html'),
+      },
+    },
+  },
 })
